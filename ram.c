d3c5b483fd18b74bbe8080f60fc7f9dbb05e5c67
Lithinium <lithinium67@gmail.com>
2024-11-26 20:29:33 -0300

    Full Feature Implemented

--- ram.c
+++ ram.c
@@ -1,39 +1,89 @@
 #include <stdio.h>
-//#include <string.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-#define MAX_BUFF 25
+#include "ram.h"
 
-long getTotalRam(void){
+#define MAX_BUFF 64
+
+unsigned long getTotalRam(void){
    
-    FILE *meminfo = fopen("/proc/meminfo", "r");
+    FILE *memInfo = fopen("/proc/meminfo", "r");
 
-    char buffRead[MAX_BUFF];
-
-    long TotalRam;
-
-    if (!meminfo){
+    if (!memInfo){
         fprintf(stderr, "Error: Open /proc/meminfo Failed");
 
         return 2;
     }
 
-    /*
+    char buffRead[MAX_BUFF];
 
-    ToDo:
+    unsigned long totalRam;
+
+
+
+    fseek(memInfo, 16, SEEK_SET);
+
+    fgets(buffRead, sizeof(buffRead), memInfo);
+
+    fclose(memInfo);
     
-    Tomar solo los numeros de la cadena de texto.
+    int buffLength = strlen(buffRead);
 
-    + esto usando fseek() para posicionar el cursor por detras de los numeros y leer la cadena de texto
+    // remove " Kb"
+    for (int i = buffLength - 1; i >= 0; i--) {
+        if (!isdigit(buffRead[i])) {
+            buffRead[i] = '\0';
+        } else {
+            break;
+        }
+    }
 
-    convertirlo a un long long int y asignarlo a la variable TotalRam.
+    totalRam = strtol(buffRead, NULL, 10);
     
-    */
+    return totalRam;
+}
 
-    fgets(buffRead, sizeof(buffRead), meminfo);
+unsigned long getFreeRam(){
 
-    fprintf(stdout, "%s\n", buffRead);
+    FILE *memInfo = fopen("/proc/meminfo", "r");
 
-    fclose(meminfo);
+    if (!memInfo){
+        fprintf(stderr, "Error: Open /proc/meminfo Failed");
 
-    return TotalRam;
+        return 2;
+    }
+
+    char buffRead[MAX_BUFF];
+
+    unsigned long freeRam;
+
+    char* tempStr = (char *) malloc(MAX_BUFF);
+
+    fgets(tempStr, sizeof(buffRead), memInfo); // jump to second line
+
+    free(tempStr);
+
+
+    fseek(memInfo, 16, SEEK_CUR);
+
+    fgets(buffRead, sizeof(buffRead), memInfo); // read line
+
+    fclose(memInfo);
+
+    int buffLength = strlen(buffRead);
+
+    // remove " Kb"
+    for (int i = buffLength - 1; i >= 0; i--) {
+        if (!isdigit(buffRead[i])) {
+            buffRead[i] = '\0';
+        } else {
+            break;
+        }
+    }
+
+    freeRam = strtol(buffRead, NULL, 10);
+
+    return freeRam;
 }
---
+++ ram.h
@@ -0,0 +1,7 @@
+#ifndef RAM_H
+#define RAM_H
+
+unsigned long getTotalRam(void);
+unsigned long getFreeRam(void);
+
+#endif
