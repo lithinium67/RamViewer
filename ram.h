bf5e704eeff0f390456de36528dae71f09332aaa
lithinium <lithinium67@gmail.com>
2024-12-13 22:58:23 -0300

    Improve Descriptions in Header File

--- ram.h
+++ ram.h
@@ -11,8 +11,11 @@ See the LICENSE File for Details
 #ifndef RAM_H
 #define RAM_H
 
-unsigned long getTotalRam(void); // in Kb
-unsigned long getAvaibleRam(void); // in Kb
-unsigned long getUsedRam(void); // in Kb
+/* Get the Total of the RAM in KiloBytes */
+unsigned long getTotalRam(void);
+/* Get the Total of the Avaible RAM in KiloBytes */
+unsigned long getAvaibleRam(void);
+/* Get the Total of the Used RAM in KiloBytes */
+unsigned long getUsedRam(void);
 
 #endif
